75c098c77f9de8ee2d2cc155ca49b0ba728d7fa0
smallnamespace <smallnamespace@gmail.com>
2016-04-07 13:32:54 -0700

    tree.py : Make mypy happy with the type annotations

--- pymcts/tree.py
+++ pymcts/tree.py
@@ -6,34 +6,35 @@ T = TypeVar('T')
 class Node(Generic[T]):
     """An abstract multi-child tree"""
 
-    def __init__(self, value: T):
+    def __init__(self, value: T) -> None:
         self.value = value
 
-        self._children = []
-        self._parent = None
+        self._children = [] # type: List[Node[T]]
+        self._parent = None # type: Node[T]
 
     @property
-    def children(self):
-        return self.children
+    def children(self) -> List["Node[T]"]:
+        return self._children
 
     @children.setter
-    def children(self, children: List["Node"[T]]=[]):
+    def children(self, children: List["Node[T]"]=[]) -> None:
         for child in children:
             child._parent = self
         self._children = children
 
     @property
-    def parent(self):
+    def parent(self) -> "Node[T]":
         return self._parent
 
-    def add_children(self, children: List["Node"[T]]=[]):
+    def add_children(self, children: List["Node[T]"]=[]):
         self.children = self.children + children
         return self
 
-    def add_child(self, child: "Node"[T]):
+    def add_child(self, child: "Node[T]"):
         self.add_children([child])
         return self
 
 
 # Empty trees are just None
+# Note that mypy doesn't yet support this, but it's in PEP 484
 Tree = Optional[Node[T]]
