807df3054fedb62602de620e3c45b6561c40974e
smallnamespace <smallnamespace@gmail.com>
2016-04-07 13:58:41 -0700

    .travis.yml : initial config
    Also switch setup.py to spaces, add testing requirements

--- setup.py
+++ setup.py
@@ -2,29 +2,30 @@
 
 from setuptools import setup, find_packages
 
-
 CLASSIFIERS = [
-	"Development Status :: 2 - Pre-Alpha",
-	"Intended Audience :: Developers",
-	"License :: OSI Approved :: Apache Software License",
-	"Programming Language :: Python",
-	"Programming Language :: Python :: 3",
-	"Programming Language :: Python :: 3.5",
-	"Topic :: Games/Entertainment :: Turn Based Strategy",
+    "Development Status :: 2 - Pre-Alpha",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: Apache Software License",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.5",
+    "Topic :: Games/Entertainment :: Turn Based Strategy",
 ]
 
+_tests_require = ["pytest", "hypothesis"]
 
 setup(
-	name="pymcts",
-	version=0.1,
-	packages=find_packages(exclude="tests"),
-	tests_require=["pytest", "hypothesis"],
-	author="smallnamespace",
-	author_email="smallnamespace@gmail.com",
-	description="Python implementation of Monte Carlo Tree Search (MCTS)",
-	classifiers=CLASSIFIERS,
-	download_url="https://github.com/smallnamespace/pymcts/tarball/master",
-	license="ASL v2",
-	url="https://github.com/smallnamespace/pymcts",
-	zip_safe=True,
+    name="pymcts",
+    version=0.1,
+    packages=find_packages(exclude="tests"),
+    tests_require=_tests_require,
+    extras_require={"tests": _tests_require},
+    author="smallnamespace",
+    author_email="smallnamespace@gmail.com",
+    description="Python implementation of Monte Carlo Tree Search (MCTS)",
+    classifiers=CLASSIFIERS,
+    download_url="https://github.com/smallnamespace/pymcts/tarball/master",
+    license="ASL v2",
+    url="https://github.com/smallnamespace/pymcts",
+    zip_safe=True,
 )
