786f3410536b373be24a0c7c86d162ddd687d2e3
smallnamespace <smallnamespace@gmail.com>
2016-04-07 13:32:46 -0700

    tree.py : initial implementation

--- pymcts/tree.py
+++ pymcts/tree.py
@@ -1,4 +1,39 @@
-class Tree:
-    def __init__(self):
-        pass
+from typing import Generic, List, Optional, TypeVar
 
+T = TypeVar('T')
+
+
+class Node(Generic[T]):
+    """An abstract multi-child tree"""
+
+    def __init__(self, value: T):
+        self.value = value
+
+        self._children = []
+        self._parent = None
+
+    @property
+    def children(self):
+        return self.children
+
+    @children.setter
+    def children(self, children: List["Node"[T]]=[]):
+        for child in children:
+            child._parent = self
+        self._children = children
+
+    @property
+    def parent(self):
+        return self._parent
+
+    def add_children(self, children: List["Node"[T]]=[]):
+        self.children = self.children + children
+        return self
+
+    def add_child(self, child: "Node"[T]):
+        self.add_children([child])
+        return self
+
+
+# Empty trees are just None
+Tree = Optional[Node[T]]
