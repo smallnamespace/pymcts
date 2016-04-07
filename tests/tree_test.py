23ca3daeb1d1035df82bc34635a128b69fc961fc
smallnamespace <smallnamespace@gmail.com>
2016-04-07 16:02:43 -0700

    tree_test : minor style changes

--- tests/tree_test.py
+++ tests/tree_test.py
@@ -2,6 +2,18 @@ from hypothesis import given, strategies as st
 from pymcts.tree import Node, Tree
 
 tree_value_strategy = st.floats() | st.booleans() | st.text()
+# Random nodes
+node_strategy = st.builds(Node, tree_value_strategy)
+# Random trees
+tree_strategy = st.recursive(
+    node_strategy,
+    lambda children: st.builds(Node, tree_value_strategy, st.lists(children)))
+
+
+def tree_dfs_postorder(node: Node) -> Node:
+    for child in node.children:
+        yield from tree_dfs_postorder(child)
+    yield node
 
 
 @given(tree_value_strategy)
@@ -9,21 +21,8 @@ def test_value(x):
     assert Node(x).value is x
 
 
-# Random nodes
-node_strategy = st.builds(Node, tree_value_strategy)
-# Random trees
-tree_strategy = st.recursive(node_strategy,
-                             lambda children: st.builds(Node, tree_value_strategy, st.lists(children)))
-
-
-def tree_dfs(node: Node) -> Node:
-    for child in node.children:
-        yield from tree_dfs(child)
-    yield node
-
-
 @given(tree_strategy)
 def test_parent(tree: Tree):
-    for node in tree_dfs(tree):
+    for node in tree_dfs_postorder(tree):
         for child in node.children:
             assert child.parent is node
